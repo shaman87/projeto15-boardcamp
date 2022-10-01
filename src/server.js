9b72c1edb66377fbde423eb32bdce6d186437386
Felipe <rockalbuquerque@gmail.com>
2022-10-01 03:15:54 -0300

    Inserir categoria

--- src/server.js
+++ src/server.js
@@ -6,12 +6,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
-
 app.get("/categories", async (req, res) => {
     try {
         const categories = await connection.query(
-            `SELECT * FROM categories`
+            `SELECT * FROM categories;`
         );
         
         res.send(categories.rows);
@@ -22,6 +20,32 @@ app.get("/categories", async (req, res) => {
     }
 });
 
+app.post("/categories", async (req, res) => {
+    const { name } = req.body;
 
+    if(!name) {
+        return res.sendStatus(400);
+    }
+
+    try {
+        const existName = await connection.query(
+            `SELECT name FROM categories WHERE name = $1;`, [name]
+        );
+
+        if(existName.rows[0]) {
+            return res.sendStatus(409);
+        }
+
+        await connection.query(
+            `INSERT INTO categories (name) VALUES ($1);`, [name]
+        );
+
+        return res.sendStatus(201);
+
+    } catch(error) {
+        console.error(error);
+        return res.sendStatus(500);
+    }
+});
 
 app.listen(4000, () => console.log("Listening on port 4000"));
