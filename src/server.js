2b1739ccd50e8913455a1b630d58ae1e44835f2b
Felipe <rockalbuquerque@gmail.com>
2022-10-01 02:30:09 -0300

    Listar categorias

--- src/server.js
+++ src/server.js
@@ -1,8 +1,27 @@
 import express from "express";
 import cors from "cors";
+import { connection } from "./database/database.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+
+
+app.get("/categories", async (req, res) => {
+    try {
+        const categories = await connection.query(
+            `SELECT * FROM categories`
+        );
+        
+        res.send(categories.rows);
+
+    } catch(error) {
+        console.error(error);
+        return res.sendStatus(500);
+    }
+});
+
+
 
 app.listen(4000, () => console.log("Listening on port 4000"));
